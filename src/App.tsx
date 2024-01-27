b8c92e4185913742dd9bd29fa78c8da5235c6dc2
Tomas Tosello <tomitosello@gmail.com>
2024-01-27 12:19:10 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+
+
+function App() {
+
+  const Layout = ()=> {
+    return (
+      <div>
+        <Navbar/>
+        <div className="container">
+          <div className="menu-container">
+            <Menu/>
+          </div>
+          <div className="content-container">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Layout/>,
+      children: [
+        {
+        path: "/",
+        element:<Home/>,
+        },
+        {
+          path: "/users",
+          element:<Users/>,
+        },
+        {
+          path: "/products",
+          element:<Products/>,
+        },
+    ],
+    },
+    {
+      path: "/login",
+      element:<Login/>,
+    }
+  ]);
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import "./Home.scss";
+
+
+const Home = () => {
+  return (
+    <div className="home">Home</div>
+  )
+}
+
+export default Home
